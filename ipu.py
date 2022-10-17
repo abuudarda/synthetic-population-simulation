e7fa365cc76118dfc15c19bc5a4488cc534c9833
abuudarda <abuu.darda.ad@gmail.com>
2022-10-17 14:00:29 +0600

    ipu final

--- ipu.py
+++ ipu.py
@@ -2,52 +2,75 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df=pd.read_csv('dummy-ipu.csv')
-ipf=np.array([35.,65.,91.,65.,104.])
-# print (len(df))
-# for index, row in df.iterrows():
-#     print(row['h1'], row['h2'])
 
-# for i in range(1,len(df.columns)):
-#     print(i)
-e=epsilon=np.finfo('float').eps
-for i in ipf:
-    if i==0: i=e
-w=np.array(df['weights'],dtype=np.float32)
-df=df.drop(['ID'],axis=1)
-df=df.drop(['weights'],axis=1)
-ws=np.zeros(5)
-i=0
+def ipu(datafile, constrains):
+    df = pd.read_csv(datafile)
+    ipf = constrains
 
-for col in df.columns:
-    x=0
-    for j in range(len(df)):
-        x+=w[j]*df[col][j]
-    ws[i]=x
-    i+=1
-print('weighted sums = ',ws)
-a=df.to_numpy(dtype=np.float32)
-print(a.shape)
-print(df.shape)
-print(a)
-print(type(a[1][1]))
-# print(a[4][5])
-# 1=5
-# 0=8
-for i in range(df.shape[1]):
-    pws=ws[i]
-    ws[i]=ipf[i]
-    multiplier=ws[i]/pws;
-    # print(multiplier)
-    for j in range(df.shape[0]):
-        if(a[j][i]==0):continue
-        w[j]*=multiplier
-    for k in range(df.shape[1]):
-        x=0
-        for j in range(df.shape[0]):
-            x+=a[j][k]*w[j]
-        ws[k]=x
-    print('weights',i,w)
-    print("weighted sum",i+1,ws)
+    e = np.finfo('float').eps
+    for i in ipf:
+        if i == 0:
+            i = e
+    w = np.array(df['weights'], dtype=np.float32)
+    df = df.drop(['ID'], axis=1)
+    df = df.drop(['weights'], axis=1)
+    ws = np.zeros(5)
+    i = 0
+
+    for col in df.columns:
+        x = 0
+        for j in range(len(df)):
+            x += w[j]*df[col][j]
+        ws[i] = x
+        i += 1
+    print('weighted sums = ', ws)
+
+    delta = sum((abs(ws-ipf))/ipf)
+    print(delta)
+
+    a = df.to_numpy(dtype=np.float32)
+    # print(a.shape)
+    # print(df.shape)
+    print(a)
+    print(type(a[1][1]))
+
+    w1 = open("ipu-weights.txt", "w")
+    w2 = open("ipu-weighted_sums.txt", "w")
+    
+    t=1
+
+    while(True):
+
+        delta_prev = delta
+
+        w1.write('iteration'+str(t)+'\n')
+        w2.write('iteration'+str(t)+'\n')
+
+        for i in range(df.shape[1]):
+            pws = ws[i]
+            ws[i] = ipf[i]
+            multiplier = ws[i]/pws
+            # print(multiplier)
+        
+            for j in range(df.shape[0]):
+                if(a[j][i] == 0):
+                    continue
+                w[j] *= multiplier
+        
+            for k in range(df.shape[1]):
+                x = 0
+                for j in range(df.shape[0]):
+                    x += a[j][k]*w[j]
+                ws[k] = x
+        
+            w1.write('weights'+str(i)+str(w)+'\n')
+            w2.write("weighted sum"+str(i+1)+str(ws)+'\n')
+
+        delta = sum((abs(ws-ipf))/ipf)
+        if(abs(delta_prev-delta) < .0001):
+            break
+        t+=1
 
 
+ipf = np.array([35., 65., 91., 65., 104.])
+x = ipu('dummy-ipu.csv', ipf)
